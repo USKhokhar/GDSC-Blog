63ea43801e4f168a78ec3680ae66359b006159f8
USKhokhar <usk172004@gmail.com>
2022-09-11 03:03:24 +0530

    final--commit

--- script.js
+++ script.js
@@ -28,18 +28,21 @@ mainJs.addEventListener('click', () => {
 
 // homeCarousel
 const domainName = [
+    'Web Development',
     'App Development',
     'UI/UX Design',
     'Machine Learning'
 ]
 
 const domainDescription = [
+    "In this domain, we concentrate on developing and maintaining websites to solve real time problems. For any website, it's the work that happens behind the scenes to make a website look great, work fast and perform well with a seamless user experience.",
     'Mobile app development has been steadily growing, in revenues and jobs created. In this domain and all others, we encourage all to keep practising and be their best selves.',
     'In this domain, We provide designing team who with their apex of creativity immerse themselves into fantasy and fiction and pour out the elegance.',
     'Mobile app development has been steadily growing, in revenues and jobs created. In this domain and all others, we encourage all to keep practising and be their best selves.'
 ]
 
 const bannerImgs = [
+    './assets/web-dev.svg',
     './assets/app-dev-banner.svg',
     './assets/ux-ui-banner.svg',
     './assets/ml-banner.svg'
@@ -54,7 +57,7 @@ homeChange = () => {
 timerId  = () => {
     setInterval(homeChange, 1000);
     i++;
-    if(i === 3){
+    if(i === 4){
         clearInterval(homeChange);
         i = 0;
     }
